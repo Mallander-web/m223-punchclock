eee0fd15cbd970ad9389c99f2f2d5bd4e71c8884
Mallander-WEB <walter.schneider@lernende.bbw.ch>
2020-11-25 06:54:46 -0800

    Add Security

---
+++ src/main/java/ch/zli/m223/punchclock/domain/Role.java
@@ -0,0 +1,18 @@
+package ch.zli.m223.punchclock.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+
+}
