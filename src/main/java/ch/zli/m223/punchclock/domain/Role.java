dd6a8f6a58b3533dd825b28b3c973d1f72a3ea8b
Walter Schneider <weschneider02@gmail.com>
2020-11-26 09:12:08 +0100

    Add Login

--- src/main/java/ch/zli/m223/punchclock/domain/Role.java
+++ src/main/java/ch/zli/m223/punchclock/domain/Role.java
@@ -7,11 +7,43 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<ApplicationUser> applicationUsers;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ApplicationUser> getApplicationUsers() {
+        return applicationUsers;
+    }
+
+    public void setApplicationUsers(List<ApplicationUser> applicationUsers) {
+        this.applicationUsers = applicationUsers;
+    }
+
 
 
 
--- src/main/java/ch/zli/m223/punchclock/security/WebSecurity.java
+++ src/main/java/ch/zli/m223/punchclock/security/WebSecurity.java
@@ -27,14 +27,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+        http.cors().and().csrf().disable()
+                .headers().frameOptions().disable().and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .antMatchers("**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-                // this disables session creation on Spring Security
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
     }
 
     @Override
